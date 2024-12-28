fd5c2ddabda157760ad5cbe34485d9410b30cf64
timofeyV <tivarus@ya.ru>
2024-12-28 17:05:21 +0300

    create

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+           
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        c = a + b;
+    }
+}
---
+++ src/test/java/MyTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyTest {
+
+    @Test
+    public void sumTest() {
+        int c = (2 + 3);
+        assertEquals(5, c);
+    }
+}
